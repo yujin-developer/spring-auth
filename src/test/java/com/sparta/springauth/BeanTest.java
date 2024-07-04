efe45b677a204e68badb9f3f0b44d2d0ec2339f4
YUJIN <yujin.jeon.developer@gmail.com>
2024-07-04 09:39:49 +0900

    2 Beans of same type

---
+++ src/test/java/com/sparta/springauth/BeanTest.java
@@ -0,0 +1,22 @@
+package com.sparta.springauth;
+
+import com.sparta.springauth.food.Food;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BeanTest {
+
+    @Autowired
+    @Qualifier("pizza")
+    Food food;
+
+
+    @Test@DisplayName("TEST")
+    void test1() {
+        food.eat();
+    }
+}
